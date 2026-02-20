5181b021878032d7d592f8c8025aa13d282869d5
Vlad Pavliuk <vladpavliuk@gmail.com>
2026-02-20 21:10:40 +0100

    Added new future.html and index.html, urls.py, views.py corrected

--- firstDjiango/main/urls.py
+++ firstDjiango/main/urls.py
@@ -1,7 +1,8 @@
 from django.urls import path
 
-from main.views import index_view
+from main.views import future_view, index_view
 
 urlpatterns = [
     path('', index_view, name='index'),
+    path('future/', future_view, name='future'),
 ]
--- firstDjiango/main/views.py
+++ firstDjiango/main/views.py
@@ -3,3 +3,7 @@ from django.shortcuts import render
 # Create your views here.
 def index_view(request):
     return render(request, 'main/index.html',{})
+
+
+def future_view(request):
+    return render(request, 'main/future.html', {})
