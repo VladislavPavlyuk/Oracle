bb5f8c1ed214756bb67a7568a685aaf4aeb5eea5
Vlad Pavliuk <vladpavliuk@gmail.com>
2026-02-25 09:03:34 +0100

    Errors corrected

--- firstDjiango/main/views.py
+++ firstDjiango/main/views.py
@@ -1,9 +1,13 @@
 from django.shortcuts import render
-
+import random
 # Create your views here.
 def index_view(request):
+    context = {
+        'first_value':'first message',
+        'second_value': f'Random number: {random.randint(1,100)}',
+        'range': range(1, 21)
+    }
     return render(request, 'main/index.html',{})
-
 
 def future_view(request):
     return render(request, 'main/future.html', {})
